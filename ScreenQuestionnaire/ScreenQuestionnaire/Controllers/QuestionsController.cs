cdfbc295ebfa5d8ca11f1e448c26f0f10d0e7dce
ptarsus <40682672+ptarsus@users.noreply.github.com>
2018-06-29 00:18:03 -0500

    Screen Questionnaire source files

---
+++ ScreenQuestionnaire/ScreenQuestionnaire/Controllers/QuestionsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScreenQuestionnaire.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        // GET: Questions
+        [Route("Questions")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ScreenQuestionnaire/ScreenQuestionnaire/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScreenQuestionnaire.MvcApplication" Language="C#" %>
